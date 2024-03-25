64fef044daec50d626f24b3227781b42c124cf67
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-03-25 07:33:25 +0600

    fixed some design problem in blog component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import Header from "./Component/Header/Header";
 function App() {
   return (
     <div className="md:w-4/5 mx-auto">
-      <Header></Header>
-      <div>
+      <header>
+        <Header></Header>
+      </header>
+
+      <main>
         <Blogs></Blogs>
-      </div>
+      </main>
     </div>
   );
 }
--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
+import { CiBookmark } from "react-icons/ci";
 const Blog = ({ blog }) => {
   const { cover, title, author_img, author, posted_date, reading_time, hashtags } = blog;
-  console.log(blog);
 
   return (
-    <div className="w-2/3 my-8 mb-3">
+    <div className="p-5 md:p-0 md:w-2/3 my-8 mb-3">
       <img className="w-full rounded-md " src={cover} alt="cover Image" />
 
       <div className="md:flex justify-between items-center my-4 mt-5">
@@ -17,7 +17,9 @@ const Blog = ({ blog }) => {
         </div>
 
         <div>
-          <h2 className="text-xl  text-gray-500">{reading_time} min read</h2>
+          <h2 className="text-xl  text-gray-500 flex items-center gap-1">
+            {reading_time} min read <CiBookmark className="text-xl  text-gray-500 " />{" "}
+          </h2>
         </div>
       </div>
 
@@ -26,8 +28,10 @@ const Blog = ({ blog }) => {
       </div>
 
       <div className="flex gap-3 text-xl  text-gray-500">
-        {hashtags.map((item) => (
-          <h2># {item}</h2>
+        {hashtags.map((item, idx) => (
+          <span key={idx}>
+            <a href=""># {item}</a>
+          </span>
         ))}
       </div>
       <button className=" text-indigo-800 underline decoration-1 text-xl  my-3">Mark as read</button>
